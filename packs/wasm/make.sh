d1231c25272d586d48b617c2f23cbe845e2ec02f
Jorge Prendes <jorge.prendes@gmail.com>
2021-10-31 01:22:49 +0100

    Fix wasm packaging

--- packs/wasm/make.sh
+++ packs/wasm/make.sh
@@ -2,7 +2,7 @@
 
 mkdir -p wasm
 
-cp ../../llvm/bin/llvm-box.wasm ./
-cp ../../binaryen/bin/binaryen-box.wasm ./
-cp ../../pyodide/pyodide.asm.wasm ./
-cp ../../pyodide/pyodide.asm.data ./
+cp ../../llvm/bin/llvm-box.wasm ./wasm/
+cp ../../binaryen/bin/binaryen-box.wasm ./wasm/
+cp ../../pyodide/pyodide.asm.wasm ./wasm/
+cp ../../pyodide/pyodide.asm.data ./wasm/
