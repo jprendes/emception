da323f97400492fcfdaef2dd7eaf3868f60c26b4
Jorge Prendes <jorge.prendes@gmail.com>
2023-02-16 23:33:04 +0100

    Fix subprocess output not being captured

--- packs/cpython/sitecustomize.py
+++ packs/cpython/sitecustomize.py
@@ -11,8 +11,8 @@ def rmtree(path):
                 const stat = Module.FS.stat(path);
                 if (Module.FS.isDir(stat.mode)) {{
                     for (const name of Module.FS.readdir(path)) {{
-                        if (name !== "." && name !== "..") {{
-                            rmtree(`${{path}}/${{name}}`);
+                        if (name !== '.' && name !== '..') {{
+                            rmtree(path + '/' + name);
                         }}
                     }}
                     Module.FS.rmdir(path);
@@ -36,10 +36,21 @@ sys.modules['ctypes'] = types.ModuleType('ctypes', 'ctypes stub module');
 class Popen(object):
     args = None;
     returncode = 0;
-    stdout = "";
-    stderr = "";
+    stdout = '';
+    stderr = '';
 
-    def __init__(self, arguments, stdout = None, stderr = None, env = None, cwd=None, *args, **kwargs):
+    def __init__(self, arguments,
+        stdout = None,
+        stderr = None,
+        env = None,
+        cwd=None,
+        text=None,
+        universal_newlines=None,
+        encoding=None,
+        errors=None,
+        *args,
+        **kwargs
+    ):
         self.args = arguments;
         data = json.loads(_emception.eval(f'''
             Module.onrunprocess(
@@ -50,13 +61,23 @@ class Popen(object):
                 }},
             )
         '''));
-        self.returncode = data["returncode"];
-        self.stdout = data["stdout"];
-        self.stderr = data["stderr"];
-        if callable(getattr(stdout, "write", None)):
-            stdout.write(self.stdout);
-        if callable(getattr(stderr, "write", None)):
-            stdout.write(self.stderr);
+        self.returncode = data['returncode'];
+        self.stdout = data['stdout'];
+        self.stderr = data['stderr'];
+
+        if stdout is None:
+            stdout = sys.stdout;
+        if stderr is None:
+            stderr = sys.stderr;
+
+        if callable(getattr(stdout, 'write', None)):
+            stdout.write(self.stdout);        
+        if callable(getattr(stderr, 'write', None)):
+            stderr.write(self.stderr);
+        
+        if not (text or universal_newlines or encode or error):
+            self.stdout = self.stdout.encode('utf-8');
+            self.stderr = self.stderr.encode('utf-8');
 
     def __enter__(self):
         return self;
@@ -65,10 +86,17 @@ class Popen(object):
     def poll(self, *args, **kwargs):
         return self.returncode;
     def communicate(self, *args, **kwargs):
-        return [self.stdout, self.stderr];
+        return self.stdout, self.stderr;
     def wait(self, *args, **kwargs):
         return self.returncode;
     def kill(self, *args, **kwargs):
         pass;
 
 subprocess.Popen = Popen;
+
+import warnings;
+
+def noop(*args, **kwargs):
+    pass;
+
+warnings.warn = noop;
